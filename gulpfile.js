5fbf4a217894b797d44ea94d8f62e5a926e5a523
dsxbb <13192872690@163.com>
2015-05-05 22:47:51 +0800

    add layout

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('style', function() {
 });
 
 gulp.task('script', function() {
-	return gulp.src('./public/js/*.js')
+	return gulp.src('./public/js/**/*.js')
 		.pipe(connect.reload());
 });
 
@@ -31,9 +31,9 @@ gulp.task('connect', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./public/js/*.js', ['script']);
+    gulp.watch('./public/js/**/*.js', ['script']);
     gulp.watch('./src/css/*.less', ['style']);
     gulp.watch(['./public/*.html','./*.html'], ['html']);
 });
 
-gulp.task('default', ['style','connect','watch']);
+gulp.task('default', ['style','connect','watch','script']);
--- public/js/controller/app.js
+++ public/js/controller/app.js
@@ -3,8 +3,31 @@
  * @author waterbear
  * @type {[type]}
  */
-var app = angular.module('easyToDo', ['ngMaterial']);
+var app = angular.module('easyToDo', ['ngMaterial', 'ngAria']);
+app.config(['$mdThemingProvider', function($mdThemingProvider) {
+	$mdThemingProvider
+		.theme('default')
+		.accentPalette('amber', {
+      		default: '700'
+    	});
+}]);
 
-app.controller('categoriesCtrl', function(){
-	
+app.controller('categoriesCtrl', function($scope){
+	$scope.parent = {
+		content:null
+	}
+});
+
+app.controller('allTaskCtrl', function($scope, $mdDialog){
+	$scope.messages = [
+    	{id: 1, title: "Message A", selected: false},
+    	{id: 2, title: "Message B", selected: true},
+    	{id: 3, title: "Message C", selected: true},
+    ];
+    $scope.toppings = [
+	    { name: 'Pepperoni', wanted: true },
+	    { name: 'Sausage', wanted: true },
+	    { name: 'Black Olives', wanted: true },
+	    { name: 'Green Peppers', wanted: true }
+    ];
 });
