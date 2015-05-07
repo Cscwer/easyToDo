b78a2f02ca798b1d8025a2678ff6411c4f6d44bc
dsxbb <13192872690@163.com>
2015-05-07 19:50:36 +0800

    初步完成界面布局

--- public/js/controller/app.js
+++ public/js/controller/app.js
@@ -15,19 +15,32 @@ app.config(['$mdThemingProvider', function($mdThemingProvider) {
 app.controller('categoriesCtrl', function($scope){
 	$scope.parent = {
 		content:null
-	}
+	};
+	$scope.user = {
+		email: ''
+	};
 });
 
 app.controller('allTaskCtrl', function($scope, $mdDialog){
+	$scope.allTasks = [
+		{id: 1, title: "Message A", selected: false},
+    	{id: 2, title: "Message B", selected: false},
+    	{id: 3, title: "Message C", selected: false},
+    	{ title: 'Pepperoni', selected: true },
+	    { title: 'Sausage', selected: true },
+	    { title: 'Black Olives', selected: true },
+	    { title: 'Green Peppers', selected: true }
+	];
+
 	$scope.messages = [
     	{id: 1, title: "Message A", selected: false},
-    	{id: 2, title: "Message B", selected: true},
-    	{id: 3, title: "Message C", selected: true},
+    	{id: 2, title: "Message B", selected: false},
+    	{id: 3, title: "Message C", selected: false},
     ];
     $scope.toppings = [
-	    { name: 'Pepperoni', wanted: true },
-	    { name: 'Sausage', wanted: true },
-	    { name: 'Black Olives', wanted: true },
-	    { name: 'Green Peppers', wanted: true }
+	    { title: 'Pepperoni', wanted: true },
+	    { title: 'Sausage', wanted: true },
+	    { title: 'Black Olives', wanted: true },
+	    { title: 'Green Peppers', wanted: true }
     ];
 });
