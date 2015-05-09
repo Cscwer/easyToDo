ccbcca5e809acb21af4e700316a1f2ae8ad99e18
dsxbb <13192872690@163.com>
2015-05-09 09:07:18 +0800

    增加按钮显示

--- public/js/controller/app.js
+++ public/js/controller/app.js
@@ -12,20 +12,37 @@ app.config(['$mdThemingProvider', function($mdThemingProvider) {
     	});
 }]);
 
-app.controller('categoriesCtrl', function($scope){
-	$scope.parent = {
-		content:null
-	};
+/**
+ * 主控制器
+ * @param  {[type]} $scope [description]
+ * @return {[type]}        [description]
+ */
+app.controller('easyToDoCtrl', function($scope){
+	
+	/** 任务分类 */
+	$scope.categories = [
+
+		{content: '百度 IFE 项目'},
+		{content: '毕业设计'},
+		{content: '社团活动'},
+		{content: '家庭分类'}
+	];
+
+	$scope.cateChange = function(index) {
+		$scope.cateItem = index;
+	}
+
+
 	$scope.user = {
 		email: ''
 	};
-});
 
-app.controller('allTaskCtrl', function($scope, $mdDialog){
+	/** 所有任务 */
 	$scope.allTasks = [
-		{id: 1, title: "Message A", selected: false},
-    	{id: 2, title: "Message B", selected: false},
-    	{id: 3, title: "Message C", selected: false},
+		
+    	{ title: "Message B", selected: false},
+    	{ title: "A", selected: false},
+    	{ title: "Message C", selected: false},
     	{ title: 'Pepperoni', selected: true },
 	    { title: 'Sausage', selected: true },
 	    { title: 'Black Olives', selected: true },
@@ -33,9 +50,9 @@ app.controller('allTaskCtrl', function($scope, $mdDialog){
 	];
 
 	$scope.messages = [
-    	{id: 1, title: "Message A", selected: false},
-    	{id: 2, title: "Message B", selected: false},
-    	{id: 3, title: "Message C", selected: false},
+    	{ title: "Message A", selected: false},
+    	{ title: "Message B", selected: false},
+    	{ title: "Message C", selected: false},
     ];
     $scope.toppings = [
 	    { title: 'Pepperoni', wanted: true },
@@ -43,4 +60,27 @@ app.controller('allTaskCtrl', function($scope, $mdDialog){
 	    { title: 'Black Olives', wanted: true },
 	    { title: 'Green Peppers', wanted: true }
     ];
+    $scope.taskChange = function(index) {
+    	$scope.taskItem = index;
+    }
+
+
+
+
+    /** 任务详情 */
+
+});
+
+app.controller('allTasksCtrl', function($scope) {
+	/** 所有任务 */
+	$scope.allTasks = [
+		{ title: "Message A", selected: false},
+    	{ title: "Message B", selected: false},
+    	{ title: "Message C", selected: false},
+    	{ title: 'Pepperoni', selected: true },
+	    { title: 'Sausage', selected: true },
+	    { title: 'Black Olives', selected: true },
+	    { title: 'Green Peppers', selected: true }
+	];
+
 });
